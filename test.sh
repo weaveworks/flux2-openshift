37a21f79fb3b3dc479789e28f124fe1e7cf424dd
Chanwit Kaewkasi <chanwit@gmail.com>
2021-06-04 22:38:49 +0700

    test v0.14.2

--- test.sh
+++ test.sh
@@ -1,12 +1,16 @@
-make release
-make opm-index
+# make release
+# make opm-index
 
 ekz create cluster
 
 operator-sdk olm install
 
 ls -d test/* | xargs -I{} kubectl apply -f {}
-kubect get installplan -A
-kubect get csv -A
+
+INSTALL_PLAN=$(kubectl get installplan -n flux-system -oyaml | yq e .items[].metadata.name -)
+
+kubectl wait --for=condition=Installed=true installplan/$INSTALL_PLAN -n flux-system 
+
+kubectl get installplan -A
+kubectl get csv -A
 kubectl get pods -n flux-system
-watch kubectl get pods -n flux-system
