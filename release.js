dfec927d7a87513cc7d69cd1bc9208dcf3c2c67e
Chanwit Kaewkasi <chanwit@gmail.com>
2021-06-18 19:29:48 +0700

    update flux to v0.15.1

--- release.js
+++ release.js
@@ -91,6 +91,7 @@ documents
 // Update ClusterServiceVersion
 csv.spec.install.spec.deployments = deployments
 csv.metadata.name = `flux.v${version}`
+csv.metadata.annotations.containerImage = "ghcr.io/fluxcd/source-controller:v0.15.0"
 csv.spec.version = version
 csv.spec.minKubeVersion = "1.16.0"
 csv.spec.maturity = "stable"
