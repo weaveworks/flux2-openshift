6de9ceab99f0a21cc4dda24d9952d5023928ca26
Chanwit Kaewkasi <chanwit@gmail.com>
2021-12-01 20:59:13 +0700

    parameterized container image
    Signed-off-by: Chanwit Kaewkasi <chanwit@gmail.com>

--- release.sh
+++ release.sh
@@ -16,8 +16,10 @@ echo "Patch to remove fsGroup with Kustomize ..."
 # require kustomize 4.1.3
 kustomize build . > "$manifest"
 
+source_controller_image=$(yq e '.spec.template.spec.containers[].image' gotk-components.yaml | grep source)
+
 echo "Calling release js ..."
-./release.js "$manifest" "$version"
+./release.js "${manifest}" "${version}" "${source_controller_image}"
 
 echo "Bundle with operator-sdk ..."
 operator-sdk bundle validate --select-optional name=operatorhub --verbose "flux/$version"
